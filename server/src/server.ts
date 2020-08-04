fbcde353317b08c057e4839f647d5886c1a7d455
Hugo Fusinato <hugohvf@hotmail.com>
2020-08-04 13:33:55 -0300

    Setting server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,21 @@
-console.log('vamo')
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+
+
+    return response.json({welcomeTo: "Proffy server"})
+})
+
+app.get('/users', (request, response) => {
+    const users = [
+        {name: "Hugo"}
+    ]
+
+    return response	.json(users)
+})
+
+app.listen(3333);
